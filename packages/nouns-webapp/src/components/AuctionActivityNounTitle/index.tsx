2005d3aa780bef094d8b9d874280190178d1a60e
sora_shrlock <soradaibu141221@gmail.com>
2023-02-19 20:13:20 +0900

    fix Trans by adding & adjusting locales

--- packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
@@ -1,12 +1,13 @@
 import { BigNumber } from 'ethers';
 import classes from './AuctionActivityNounTitle.module.css';
+import { Trans } from '@lingui/macro';
 
 const AuctionActivityNounTitle: React.FC<{ unounId: BigNumber; isCool?: boolean }> = props => {
   const { unounId, isCool } = props;
   return (
     <div className={classes.wrapper}>
       <h1 style={{ color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)' }}>
-        UNoun {unounId.toString()}
+        <Trans>UNoun {unounId.toString()}</Trans>
       </h1>
     </div>
   );
