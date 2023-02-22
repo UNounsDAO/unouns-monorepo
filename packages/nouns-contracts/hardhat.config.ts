78929ea81fb139c98c55a6de6a29d3e61d7f17a7
sora_shrlock <soradaibu141221@gmail.com>
2023-02-23 07:23:08 +0900

    feat: mainnet deployment

--- packages/nouns-contracts/hardhat.config.ts
+++ packages/nouns-contracts/hardhat.config.ts
@@ -25,7 +25,9 @@ const config: HardhatUserConfig = {
   networks: {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
